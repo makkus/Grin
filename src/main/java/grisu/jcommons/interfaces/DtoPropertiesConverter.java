a453e1daa2ef362fa83b84b9208732a1d7124866
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2013-03-21 14:30:46 +1300

    Converting all DtoProperties to String

--- src/main/java/grisu/jcommons/interfaces/DtoPropertiesConverter.java
+++ src/main/java/grisu/jcommons/interfaces/DtoPropertiesConverter.java
@@ -1,10 +1,12 @@
 package grisu.jcommons.interfaces;
 
-import java.util.Map;
-
 import grisu.model.info.dto.DtoProperties;
 
+import java.util.Map;
+
 import org.dozer.CustomConverter;
+
+import com.google.common.collect.Maps;
 
 public class DtoPropertiesConverter implements CustomConverter {
 
@@ -16,6 +18,13 @@ public class DtoPropertiesConverter implements CustomConverter {
 		if ( sourceFieldValue == null ) {
 			return null;
 		}
+		Map<Object, Object> props = (Map)sourceFieldValue;
+		
+		Map<String, String> newMap = Maps.newLinkedHashMap();
+		for(Map.Entry entry : props.entrySet()) {
+		   newMap.put(entry.getKey().toString(), entry.getValue().toString());
+		}
+
 
 		return DtoProperties.createProperties((Map<String, String>) sourceFieldValue);
 
